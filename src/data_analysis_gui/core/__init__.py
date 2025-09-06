edaf7ba77c92d812d632bed228d4032f11568ca9
ck852 <134101098+ck852@users.noreply.github.com>
2025-09-06 19:10:44 -0400

    restored batch_processor; further structural changes incoming

--- src/data_analysis_gui/core/__init__.py
+++ src/data_analysis_gui/core/__init__.py
@@ -4,6 +4,6 @@ Core business logic module for the data analysis GUI.
 """
 
 from .channel_definitions import ChannelDefinitions
-from .current_density_exporter import CurrentDensityExporter
+
 
 __all__ = ['ChannelDefinitions', 'CurrentDensityExporter']
