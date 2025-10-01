e875a6750cda5db8a924da73c3305d80cea122fb
ck852 <134101098+ck852@users.noreply.github.com>
2025-10-01 08:51:19 -0400

    wcp loader in init

--- src/data_analysis_gui/core/loaders/__init__.py
+++ src/data_analysis_gui/core/loaders/__init__.py
@@ -5,5 +5,6 @@ License: MIT (see LICENSE file for details)
 """
 
 from .abf_loader import load_abf, validate_abf_file
+from .wcp_loader import load_wcp
 
-__all__ = ["load_abf", "validate_abf_file"]
+__all__ = ["load_abf", "validate_abf_file", 'load_wcp']
